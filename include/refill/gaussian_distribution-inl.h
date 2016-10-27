f6b2d7da1395dc864359c370af4591fa410728a6
Igor Gilitschenski <igor@gilitschenski.org>
2016-10-28 01:16:10 +0200

    Small fix.

--- include/refill/gaussian_distribution-inl.h
+++ include/refill/gaussian_distribution-inl.h
@@ -7,8 +7,8 @@ namespace refill {
 
 template <int DIM>
 GaussianDistribution<DIM>::GaussianDistribution()
-    : mean_(Eigen::VectorXd::Zero(1)),
-      covmat_(Eigen::MatrixXd::Identity(1, 1)) {}
+    : mean_(Eigen::VectorXd::Zero(DIM)),
+      covmat_(Eigen::MatrixXd::Identity(DIM, DIM)) {}
 
 template <int DIM>
 GaussianDistribution<DIM>::GaussianDistribution(Eigen::VectorXd dist_mean,
@@ -17,12 +17,14 @@ GaussianDistribution<DIM>::GaussianDistribution(Eigen::VectorXd dist_mean,
 }
 
 template <int DIM>
-void GaussianDistribution<DIM>::SetDistParam(Eigen::VectorXd dist_mean,
-                                        Eigen::MatrixXd dist_cov) {
-  CHECK_EQ(dist_mean.size(), dist_cov.rows());
-  CHECK_EQ(dist_cov.rows(), dist_mean.cols());
+void GaussianDistribution<DIM>::SetDistParam(Eigen::Matrix<double,DIM,1> dist_mean,
+                                        Eigen::Matrix<double,DIM,DIM> dist_cov) {
+  if(DIM == Eigen::Dynamic) {
+    CHECK_EQ(dist_mean.size(), dist_cov.rows());
+    CHECK_EQ(dist_cov.rows(), dist_mean.cols());
+  }
 
-  Eigen::LLT<Eigen::MatrixXd> chol_of_cov(dist_cov);
+  Eigen::LLT<Eigen::Matrix<double,DIM,DIM>> chol_of_cov(dist_cov);
   CHECK(chol_of_cov.info() != Eigen::NumericalIssue) << "Matrix not s.p.d.";
 
   mean_ = dist_mean;
@@ -32,9 +34,11 @@ void GaussianDistribution<DIM>::SetDistParam(Eigen::VectorXd dist_mean,
 template <int DIM>
 GaussianDistribution<DIM> GaussianDistribution<DIM>::operator+(
     const GaussianDistribution<DIM>& right_side) {
-  CHECK(right_side.dim() == dim()) << "Distribution dimensions do not match.";
-  GaussianDistribution result(mean_ + right_side.mean(),
-                              covmat_ + right_side.cov());
+  if(DIM == Eigen::Dynamic) {
+    CHECK(right_side.dim() == dim()) << "Distribution dimensions do not match.";
+  }
+  GaussianDistribution<> result(mean_ + right_side.mean(),
+                                covmat_ + right_side.cov());
   return result;
 }
 
--- include/refill/gaussian_distribution.h
+++ include/refill/gaussian_distribution.h
@@ -11,22 +11,23 @@ class GaussianDistribution {
   GaussianDistribution();
   GaussianDistribution(Eigen::VectorXd dist_mean, Eigen::MatrixXd dist_cov);
 
-  void SetDistParam(Eigen::VectorXd dist_mean, Eigen::MatrixXd dist_cov);
+  void SetDistParam(Eigen::Matrix<double, DIM, 1> dist_mean,
+                    Eigen::Matrix<double, DIM, DIM> dist_cov);
 
-  Eigen::MatrixXd cov() const { return covmat_; }
+  Eigen::Matrix<double, DIM, DIM> cov() const { return covmat_; }
   int dim() const { return mean_.size(); }
-  Eigen::VectorXd mean() const { return mean_; }
+  Eigen::Matrix<double, DIM, 1> mean() const { return mean_; }
 
   GaussianDistribution operator+(const GaussianDistribution& right_side);
 
  private:
-  Eigen::MatrixXd covmat_;
-  Eigen::VectorXd mean_;
+  Eigen::Matrix<double, DIM, DIM> covmat_;
+  Eigen::Matrix<double, DIM, 1> mean_;
 };
 
 // Non-member operator overloading.
 GaussianDistribution<> operator*(const Eigen::MatrixXd& mat,
-                               const GaussianDistribution<> gaussian);
+                                 const GaussianDistribution<> gaussian);
 
 }  // namespace refill
 
