3eb6fdde44acfab63e75f01cd86df866e25a9d19
jwidauer <jakob.widauer@gmail.com>
2017-03-17 16:21:10 +0100

    Changed the system model base test.

--- src/tests/system_model_base_test.cc
+++ src/tests/system_model_base_test.cc
@@ -4,34 +4,47 @@
 
 #include <Eigen/Dense>
 
+#include "refill/distributions/gaussian_distribution.h"
+
 namespace refill {
+
+class SystemModelClass : public SystemModelBase {
+ public:
+  SystemModelClass(const size_t& state_dim,
+                   const GaussianDistribution& system_noise)
+      : SystemModelBase(state_dim, system_noise) {
+  }
+  SystemModelClass(const size_t& state_dim,
+                   const GaussianDistribution& system_noise,
+                   const size_t& input_dim)
+      : SystemModelBase(state_dim, system_noise, input_dim) {
+  }
+  Eigen::VectorXd propagate(const Eigen::VectorXd& state,
+                            const Eigen::VectorXd& input,
+                            const Eigen::VectorXd& noise) const {
+    return state + input + noise;
+  }
+  void setModelParameters(const size_t& state_dim,
+                          const DistributionInterface& system_noise) {
+    this->setSystemModelBaseParameters(state_dim, system_noise);
+  }
+  void setModelParameters(const size_t& state_dim,
+                          const DistributionInterface& system_noise,
+                          const size_t& input_dim) {
+    this->setSystemModelBaseParameters(state_dim, system_noise, input_dim);
+  }
+};
 
 TEST(SystemModelBaseTest, Fullrun) {
   constexpr size_t kStateDim = 2;
   constexpr size_t kInputDim = 2;
   constexpr size_t kNoiseDim = 2;
 
-  class SystemModelClass : SystemModelBase {
-    SystemModelClass(const size_t& state_dim,
-                     const GaussianDistribution& system_noise)
-        : SystemModelBase(state_dim, system_noise) {
-    }
-    SystemModelClass(const size_t& state_dim,
-                     const GaussianDistribution& system_noise,
-                     const size_t& input_dim)
-        : SystemModelBase(state_dim, system_noise, input_dim) {
-    }
-    Eigen::VectorXd propagate(const Eigen::VectorXd& state,
-                              const Eigen::VectorXd& input,
-                              const Eigen::VectorXd& noise) const {
-      return state + input + noise;
-    }
-  };
-
   Eigen::VectorXd mean = Eigen::VectorXd::Zero(kNoiseDim);
   Eigen::MatrixXd covariance = Eigen::MatrixXd::Identity(kNoiseDim, kNoiseDim);
   GaussianDistribution system_noise(mean, covariance);
 
+  // Test constructor with two parameters
   SystemModelClass system_model_1(kStateDim, system_noise);
 
   ASSERT_EQ(system_model_1.getStateDim(), kStateDim);
@@ -40,13 +53,7 @@ TEST(SystemModelBaseTest, Fullrun) {
   ASSERT_EQ(system_model_1.getSystemNoise()->mean(), mean);
   ASSERT_EQ(system_model_1.getSystemNoise()->cov(), covariance);
 
-  Eigen::VectorXd propagated = system_model_1.propagate(
-      Eigen::VectorXd::Ones(kStateDim), Eigen::VectorXd::Ones(kInputDim),
-      Eigen::VectorXd::Ones(kNoiseDim));
-
-  ASSERT_EQ(propagated, Eigen::VectorXd::Ones(kStateDim) * 3.0);
-
-  system_model_1.setSystemModelBaseParameters(kStateDim, system_noise);
+  system_model_1.setModelParameters(kStateDim, system_noise);
 
   ASSERT_EQ(system_model_1.getStateDim(), kStateDim);
   ASSERT_EQ(system_model_1.getInputDim(), 0);
@@ -54,12 +61,18 @@ TEST(SystemModelBaseTest, Fullrun) {
   ASSERT_EQ(system_model_1.getSystemNoise()->mean(), mean);
   ASSERT_EQ(system_model_1.getSystemNoise()->cov(), covariance);
 
-  propagated = system_model_1.propagate(Eigen::VectorXd::Ones(kStateDim),
-                                        Eigen::VectorXd::Ones(kInputDim),
-                                        Eigen::VectorXd::Ones(kNoiseDim));
+  Eigen::MatrixXd state_samples = Eigen::MatrixXd::Ones(kStateDim, 2);
+  Eigen::MatrixXd noise_samples = Eigen::MatrixXd::Ones(kNoiseDim, 2);
+  Eigen::VectorXd input = Eigen::VectorXd::Ones(kInputDim);
 
-  ASSERT_EQ(propagated, Eigen::VectorXd::Ones(kStateDim) * 3.0);
+  Eigen::MatrixXd vectorized_propagation = system_model_1.propagateVectorized(
+      state_samples, input, noise_samples);
 
+  ASSERT_EQ(kStateDim, vectorized_propagation.rows());
+  ASSERT_EQ(4, vectorized_propagation.cols());
+  ASSERT_EQ(Eigen::MatrixXd::Ones(kStateDim, 4) * 3.0, vectorized_propagation);
+
+  // Test constructor with three parameters
   SystemModelClass system_model_2(kStateDim, system_noise, kInputDim);
 
   ASSERT_EQ(system_model_2.getStateDim(), kStateDim);
@@ -68,26 +81,13 @@ TEST(SystemModelBaseTest, Fullrun) {
   ASSERT_EQ(system_model_2.getSystemNoise()->mean(), mean);
   ASSERT_EQ(system_model_2.getSystemNoise()->cov(), covariance);
 
-  propagated = system_model_2.propagate(Eigen::VectorXd::Ones(kStateDim),
-                                        Eigen::VectorXd::Ones(kInputDim),
-                                        Eigen::VectorXd::Ones(kNoiseDim));
-
-  ASSERT_EQ(propagated, Eigen::VectorXd::Ones(kStateDim) * 3.0);
-
-  system_model_2.setSystemModelBaseParameters(kStateDim, system_noise,
-                                              kInputDim);
+  system_model_2.setModelParameters(kStateDim, system_noise, kInputDim);
 
   ASSERT_EQ(system_model_2.getStateDim(), kStateDim);
   ASSERT_EQ(system_model_2.getInputDim(), kInputDim);
   ASSERT_EQ(system_model_2.getSystemNoiseDim(), kNoiseDim);
   ASSERT_EQ(system_model_2.getSystemNoise()->mean(), mean);
   ASSERT_EQ(system_model_2.getSystemNoise()->cov(), covariance);
-
-  propagated = system_model_2.propagate(Eigen::VectorXd::Ones(kStateDim),
-                                        Eigen::VectorXd::Ones(kInputDim),
-                                        Eigen::VectorXd::Ones(kNoiseDim));
-
-  ASSERT_EQ(propagated, Eigen::VectorXd::Ones(kStateDim) * 3.0);
 }
 
 }  // namespace refill
