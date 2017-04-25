549ec7eb1839a2ef54bf44a95f8ed42384e1457e
jwidauer <jakob.widauer@gmail.com>
2017-04-25 16:01:53 +0200

    Adressed comments from PR #41.

--- src/examples/kalman_filter_example.cc
+++ src/examples/kalman_filter_example.cc
@@ -38,71 +38,71 @@
  */
 
 int main(int argc, char **argv) {
-  // initialize Q and R
-  // Q = I * 2.0
-  // R = I
+  /* initialize Q and R
+   * Q = I * 2.0
+   * R = I */
   Eigen::Matrix3d system_noise_cov = Eigen::Matrix3d::Identity() * 2.0;
   Eigen::Matrix3d measurement_noise_cov = Eigen::Matrix3d::Identity() * 1.0;
 
-  // initialize v(k)
+  /* initialize v(k) */
   refill::GaussianDistribution system_noise(Eigen::Vector3d::Zero(),
                                             system_noise_cov);
-  // initialize w(k)
+  /* initialize w(k) */
   refill::GaussianDistribution measurement_noise(
       Eigen::Vector3d::Zero(), measurement_noise_cov);
 
-  // initialize the system model
+  /* initialize the system model */
   refill::LinearSystemModel system_model(Eigen::Matrix3d::Identity(),
                                          system_noise);
-  // initialize the measurement model
+  /* initialize the measurement model */
   refill::LinearMeasurementModel measurement_model(Eigen::Matrix3d::Identity(),
                                                    measurement_noise);
 
-  // initialize the initial state distribution
-  // Assumed to be at position [1, 1, 1]^T with cov[I * 5]
+  /* initialize the initial state distribution
+   * Assumed to be at position [1, 1, 1]^T with cov[I * 5] */
   refill::GaussianDistribution initial_state(Eigen::Vector3d::Ones(),
                                              Eigen::Matrix3d::Identity() * 5.0);
-  // initialize the kf with the initial state
+  /* initialize the kf with the initial state */
   refill::ExtendedKalmanFilter ekf(initial_state);
 
-  // assume that 1.0 seconds has passed and we get a position measurement at
-  // [1.5, 1.5, 1.5]^T
-  // t = 1.0
+  /* assume that 1.0 seconds has passed and we get a position measurement at
+   * [1.5, 1.5, 1.5]^T
+   * t = 1.0 */
   double dt = 1.0;
-  Eigen::Vector3d measurement_1 = Eigen::Vector3d::Constant(1.5);
+  Eigen::Vector3d measurement = Eigen::Vector3d::Constant(1.5);
 
-  // adapt the system model noise according to the time step
+  /* adapt the system model noise according to the time step */
   system_noise.setCov(system_noise_cov * dt);
-  // adapt the system model
+  /* adapt the system model */
   system_model.setSystemParameters(Eigen::Matrix3d::Identity(), system_noise);
 
-  // predict the kf to the current time
+  /* predict the kf to the current time */
   ekf.predict(system_model);
-  // update the kf with the measurement and the measurement model
-  ekf.update(measurement_model, measurement_1);
+  /* update the kf with the measurement and the measurement model */
+  ekf.update(measurement_model, measurement);
 
-  // print the current state
+  /* print the current state */
   std::cout << "State at t = 1.0:\n";
   std::cout << "Mean:\n\n" << ekf.state().mean() << "\n\n";
   std::cout << "Covariance:\n\n" << ekf.state().cov() << "\n\n";
 
-  // Assume that another 0.5 seconds have passed and another measurement
-  // is received
-  // t = 1.5
+  /* Assume that another 0.5 seconds have passed and another measurement
+   * is received
+   * t = 1.5 */
   dt = 0.5;
-  Eigen::Vector3d measurement_2 = Eigen::Vector3d::Constant(1.5);
+  measurement = Eigen::Vector3d::Constant(1.5);
 
   // adapt the system noise according to the time step
   system_noise.setCov(system_noise_cov * dt);
   // adapt the system model
   system_model.setSystemParameters(Eigen::Matrix3d::Identity(), system_noise);
 
-  // predict the kf to the current time
+  /* predict the kf to the current time */
   ekf.predict(system_model);
-  // update the kf with the measurement and the measurement model
-  ekf.update(measurement_model, measurement_2);
+  /* update the kf with the measurement and the measurement model */
+  ekf.update(measurement_model, measurement);
 
-  // print the current state
+  /* print the current state */
   std::cout << "State at t = 1.5:\n";
   std::cout << "Mean:\n\n" << ekf.state().mean() << "\n\n";
   std::cout << "Covariance:\n\n" << ekf.state().cov() << "\n\n";
